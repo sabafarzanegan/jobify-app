8ee52d26bb53dc3fef3b9f8aa75c9e4c28aebd96
saba farzanegan <saba.far.99@gmail.com>
2024-10-09 22:06:10 +0330

    add notFound page

--- src/components/main/Notfound.tsx
+++ src/components/main/Notfound.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import NOTFOUNDIMG from "../../assets/images/notfound.png";
+import { Button } from "../ui/button";
+import Container from "./Container";
 function Notfound() {
-  return <div>404</div>;
+  return (
+    <Container>
+      <section className="flex flex-col items-center gap-y-4">
+        <img src={NOTFOUNDIMG} alt="" className="w-[800px] mx-auto" />
+        <p>صفحه موردنظر یافت نشد</p>
+        <Link to="/">
+          <Button variant="outline">برگشت به صفحه اصلی</Button>
+        </Link>
+      </section>
+    </Container>
+  );
 }
 
 export default Notfound;
